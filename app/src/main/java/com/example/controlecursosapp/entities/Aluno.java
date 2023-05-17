7fd85117ef27ad1acfded33d2649a445985236dd
Isabela Resende <isabelares13@gmail.com>
2023-05-17 14:41:27 -0400

    add constructor, to string method, gets and setters on Aluno entity

--- app/src/main/java/com/example/controlecursosapp/entities/Aluno.java
+++ app/src/main/java/com/example/controlecursosapp/entities/Aluno.java
@@ -14,4 +14,65 @@ public class Aluno {
     String nomeAluno;
     String emailAluno;
     String telefoneAluno;
+
+    public int getAlunoId() {
+        return alunoId;
+    }
+
+    public void setAlunoId(int alunoId) {
+        this.alunoId = alunoId;
+    }
+
+    public int getCursoId() {
+        return cursoId;
+    }
+
+    public void setCursoId(int cursoId) {
+        this.cursoId = cursoId;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getEmailAluno() {
+        return emailAluno;
+    }
+
+    public void setEmailAluno(String emailAluno) {
+        this.emailAluno = emailAluno;
+    }
+
+    public String getTelefoneAluno() {
+        return telefoneAluno;
+    }
+
+    public void setTelefoneAluno(String telefoneAluno) {
+        this.telefoneAluno = telefoneAluno;
+    }
+
+    public Aluno() {
+    }
+
+    public Aluno(int cursoId, String nomeAluno, String emailAluno, String telefoneAluno) {
+        this.cursoId = cursoId;
+        this.nomeAluno = nomeAluno;
+        this.emailAluno = emailAluno;
+        this.telefoneAluno = telefoneAluno;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "alunoId=" + alunoId +
+                ", cursoId=" + cursoId +
+                ", nomeAluno='" + nomeAluno + '\'' +
+                ", emailAluno='" + emailAluno + '\'' +
+                ", telefoneAluno='" + telefoneAluno + '\'' +
+                '}';
+    }
 }
