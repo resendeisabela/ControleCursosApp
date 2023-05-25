4b29e0a3d0d42d1e07ec95f42e6381622dacf545
Isabela Resende <isabelares13@gmail.com>
2023-05-24 21:34:10 -0400

    removing comments

--- app/src/main/java/com/example/controlecursosapp/MainActivity.java
+++ app/src/main/java/com/example/controlecursosapp/MainActivity.java
@@ -33,9 +33,4 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
 }
